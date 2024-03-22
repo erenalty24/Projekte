021579cae77bf3c6bef4f7eafcdaa6db2f60d044
erenalty24 <150069669+erenalty24@users.noreply.github.com>
2024-03-22 10:22:23 +0100

    Calendar.py

--- import calendar.py
+++ import calendar.py
@@ -1,6 +1,6 @@
 import calendar
 
-yy = 571 #year
-mm = 2 #month
+yy = 571 #year  // Hier wo ich 571 hingeschrieben habe, kannst du auch verschiedene zahlen bzw. jahrdatum hinzufügen
+mm = 2 #month  // Das gleiche gilt auf für mm, dir ist überlassen welchen monat du wählst
 print(calendar.month(yy, mm))
 
